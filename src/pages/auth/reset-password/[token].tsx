5e4abe494becce6afe4a583af9ffb7e605f0e81d
abdelhadia72 <thisisavnb@gmail.com>
2025-02-20 08:48:59 +0100

    feat(auth): Reset password SSR

--- src/pages/auth/reset-password/[token].tsx
+++ src/pages/auth/reset-password/[token].tsx
@@ -1,13 +1,12 @@
 import Routes from '@common/defs/routes';
 import ResetPassword from '@modules/auth/components/pages/ResetPassword';
 import withAuth, { AUTH_MODE } from '@modules/auth/hocs/withAuth';
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 
 const ResetPasswordPage: NextPage = () => {
   const router = useRouter();
-  // token from url as string
   const { token } = router.query;
   return (
     <>
@@ -16,11 +15,20 @@ const ResetPasswordPage: NextPage = () => {
   );
 };
 
-export const getStaticProps = async ({ locale }: { locale: string }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['topbar', 'footer', 'leftbar', 'auth', 'common'])),
-  },
-});
+// getServerSideProps
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || 'en', [
+        'topbar',
+        'footer',
+        'leftbar',
+        'auth',
+        'common',
+      ])),
+    },
+  };
+};
 
 export default withAuth(ResetPasswordPage, {
   mode: AUTH_MODE.LOGGED_OUT,
