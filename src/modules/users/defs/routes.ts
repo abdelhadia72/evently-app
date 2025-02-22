31e635f4ca6e3a90334952e66806e5ebedcb51f1
abdelhadia72 <thisisavnb@gmail.com>
2025-02-22 15:01:21 +0100

    feat(profile): udpate the profile page

--- src/modules/users/defs/routes.ts
+++ src/modules/users/defs/routes.ts
@@ -3,7 +3,7 @@ import { CrudAppRoutes } from '@common/defs/types';
 const prefix = '/dashboard/users';
 const Routes: CrudAppRoutes = {
   ReadAll: prefix,
-  Me: '/users/me',
+  Me: '/me',
   CreateOne: prefix + '/create',
   UpdateOne: prefix + '/{id}',
 };
